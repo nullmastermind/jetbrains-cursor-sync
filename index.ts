8771ef50e9f0c7acfbe81651e7e8c7e2a7322d37
monkjiro <thenullmastermind@gmail.com>
2024-09-26 19:40:57 +0700

    feat: Integrate node-autoit-koffi for cursor control
    - Add node-autoit-koffi dependency
    - Implement basic cursor movement in index.ts
    - Update package.json with build and start scripts
    - Include build directory in .gitignore

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import autoit from "node-autoit-koffi";
+
+async function main() {
+  await autoit.init();
+  // Your code here
+  await autoit.mouseMove(0, 0);
+
+  console.log("OK");
+}
+
+void main();
